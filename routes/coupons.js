d44149f0a66d9416d074066f37736b6d71be8e34
Jethro De Guzman <jethdeguzman@gmail.com>
2019-07-21 08:46:54 +0800

    events list

--- routes/coupons.js
+++ routes/coupons.js
@@ -10,9 +10,9 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/:id', function (req, res) {
-  const redeemLink = 'http://localhost:3000/redeem/' + req.params.id
+  const redeemLink = 'http://' + process.env.EXTERNAL_IP + ':3000/redeem/' + req.params.id
   const qrLink = "https://chart.googleapis.com/chart?cht=qr&chl=" + redeemLink + "&choe=UTF-8&chs=500x500"
-  res.render('coupon', { navTitle: "Redeem Coupon", qrLink })
+  res.render('coupon', { navTitle: "Redeem Coupon", navLink: "/coupons", qrLink })
 });
 
 module.exports = router;
--- routes/events.js
+++ routes/events.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var events = require('./../data/events.json')
 
 router.get('/', function (req, res, next) {
-  res.render('events', { active: 'events' });
+  res.render('events', { active: 'events', events });
 });
 
 router.get('/:id', function (req, res) {
-  const claimLink = 'http://localhost:3000/claim/' + req.params.id
+  const claimLink = 'http://' + process.env.EXTERNAL_IP + ':3000/claim/' + req.params.id
   const qrLink = "https://chart.googleapis.com/chart?cht=qr&chl=" + claimLink + "&choe=UTF-8&chs=500x500"
-  res.render('event', { navTitle: "Claim Coupon", qrLink })
+  res.render('event', { navTitle: "Claim Coupon", navLink: "/events", qrLink })
 });
 
 module.exports = router;
